ae3f00a4ac6c755bd3656b30ae630b8e45c943a1
nivyapeter <nivyakpeter@gmail.com>
2022-06-29 23:04:07 +0530

    white block

--- apple-site/src/App.tsx
+++ apple-site/src/App.tsx
@@ -4,6 +4,8 @@ import './App.css';
 import Header from './components/Header';
 import BannerSection from './components/BannerSection';
 import applebg from "./Images/applebg.jpg";
+import MacbookAirBlock from './components/MacbookAirBlock';
+import whiteImg from "./Images/whiteImg.png";
 
 function App() {
   return (
@@ -11,11 +13,12 @@ function App() {
       <div>
       <Header />
       </div>
-      <div className='bg-white h-[10vh]'>
-        <span className='py-5 text-sm text-black'>
+      <div className='bg-white py-3 '>
+        <span className='text-sm text-black'>
                            Get 5% instant Cashback up to ₹6000 with qualifying HDFC Bank credit cards on orders over ₹54900.</span>
       </div>
-      <BannerSection style={{  backgroundImage: `url(${applebg})` }}/>
+      <BannerSection img={applebg}  title="MacBook Pro 13" subTitle="Supercharged by Apple M2 chip" linkText="Learn more" text="Buy"/>
+      <MacbookAirBlock  style={{  backgroundImage: `url(${whiteImg})`, height:"600px", }} title="MacBook Air" sub="Supercharged by M2"  text="Available in July" linkText="Learn more" buyText="View pricing"/>
     </div>
   );
 }
--- apple-site/src/components/BannerSection.tsx
+++ apple-site/src/components/BannerSection.tsx
@@ -1,28 +1,36 @@
-import React from 'react'
-
+import React from "react";
 
 type BannerProps = {
-    style:React.CSSProperties
-}
+  // style: React.CSSProperties;
+  title: string,
+  subTitle: string,
+  linkText: string,
+  text:string,
+  img: string
+};
 
-function BannerSection({style}:BannerProps) {
+function BannerSection({title,subTitle,linkText,text,img }: BannerProps) {
   return (
     <div>
-        <div className='pt-[47px] '>
-           <h2 className='text-[56px] font-semibold'></h2>
-           <h3 className='text-[28px]  font-normal'>
-            <span>Supercharged by Apple M2 chip</span>
-           </h3>
-           <div className='text-[21px] mt-3'>
-             <a className='#2997ff]' href="">Learn more</a>
-             <a  className="ml-9 text-[#2997ff]"href="">Buy</a>
-           </div>
+      <div className=" ">
+        <div className="text-white bg-black pt-12">
+          <h2 className="text-[56px] font-semibold">{title}</h2>
+          <h3 className="text-[28px]  font-normal">
+            <span>{subTitle}</span>
+          </h3>
+          <div className="text-[21px] mt-3">
+            <a className="text-[#2997ff] " href="">
+              {linkText}
+            </a>
+            <a className="ml-9 " href="">
+              {text}
+            </a>
+          </div>
         </div>
-        <div className='' style={style}>
-
-        </div>
+        <img src={img} alt="" />
+      </div>
     </div>
-  )
+  );
 }
 
-export default BannerSection
+export default BannerSection;
