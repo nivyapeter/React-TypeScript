72b71f9ab1afd5c87c8afdda3224b2f7769efefb
nivyapeter <nivyakpeter@gmail.com>
2022-07-01 23:52:51 +0530

    feat:added basic set up for turbo site

---
+++ turbu-repo-site/src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className='px-6 flex items-center max-w-[90rem] h-[4rem] mx-auto'>
+
+    </div>
+  )
+}
+
+export default Header
