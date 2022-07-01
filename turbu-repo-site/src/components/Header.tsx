0beacaff325835b2aa7085555d68a20919078c0f
nivyapeter <nivyakpeter@gmail.com>
2022-07-02 01:10:07 +0530

    header section

--- turbu-repo-site/src/components/Header.tsx
+++ turbu-repo-site/src/components/Header.tsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { ReactComponent as Logo } from "../Assets/Turbo.svg";
+import { ReactComponent as Github } from "../Assets/Github.svg";
+import { ReactComponent as Dicord} from "../Assets/Dicord.svg";
+import { ReactComponent as Hamburger} from "../Assets/Hamburger.svg";
 
 function Header() {
   return (
-    <div className='px-6 flex items-center max-w-[90rem] h-[4rem] mx-auto'>
+    <div className='px-6 flex items-center max-w-[90rem] h-[4rem] mx-auto gap-2 '>
+      <div className='flex items-center mr-2 '>
+        <Logo />
+        <a className='p-2' href=""><Github /></a>
+        <a  className='p-2'   href=""><Dicord /></a>
+        <a  className='p-2'   href=""><Hamburger /></a>
 
+      </div>
     </div>
   )
 }
