f3f7527054a011b65cd4127eeab33458db01a04d
nivyapeter <nivyakpeter@gmail.com>
2022-07-02 23:11:24 +0530

    feat: banner component

--- turbu-repo-site/src/App.tsx
+++ turbu-repo-site/src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
+import Banner from './components/Banner';
 
 function App() {
   return (
     <div className="bg-[#282c34] text-white">
       <Header />
+      <Banner  heading="Monorepos that" text="make ship happen." subText="Turborepo is a high-performance build system for JavaScript and TypeScript codebases." buttonText="Start Building " buttonTextTwo="npx create-turbo"/>
     </div>
   );
 }
--- turbu-repo-site/src/components/Header.tsx
+++ turbu-repo-site/src/components/Header.tsx
@@ -7,12 +7,13 @@ import { ReactComponent as Hamburger} from "../Assets/Hamburger.svg";
 function Header() {
   return (
     <div className='px-6 flex items-center max-w-[90rem] h-[4rem] mx-auto gap-2 '>
-      <div className='flex items-center mr-2 '>
+      <div className='flex items-center mr-2  gap-8'>
         <Logo />
+        <div className='flex items-center gap-4'>
         <a className='p-2' href=""><Github /></a>
         <a  className='p-2'   href=""><Dicord /></a>
         <a  className='p-2'   href=""><Hamburger /></a>
-
+        </div>
       </div>
     </div>
   )
