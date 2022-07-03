c7340698546be048130ebb56338835b252d87e78
nivyapeter <nivyakpeter@gmail.com>
2022-07-03 16:29:45 +0530

    fix:style issues

--- apple-site/src/App.tsx
+++ apple-site/src/App.tsx
@@ -7,6 +7,7 @@ import applebg from "./Images/applebg.jpg";
 import MacbookAirBlock from './components/MacbookAirBlock';
 import whiteImg from "./Images/whiteImg.png";
 
+
 function App() {
   return (
     <div className="App">
@@ -18,7 +19,9 @@ function App() {
                            Get 5% instant Cashback up to ₹6000 with qualifying HDFC Bank credit cards on orders over ₹54900.</span>
       </div>
       <BannerSection img={applebg}  title="MacBook Pro 13" subTitle="Supercharged by Apple M2 chip" linkText="Learn more" text="Buy"/>
-      <MacbookAirBlock  style={{  backgroundImage: `url(${whiteImg})`, height:"600px", }} title="MacBook Air" sub="Supercharged by M2"  text="Available in July" linkText="Learn more" buyText="View pricing"/>
+      
+      <MacbookAirBlock  style={{  backgroundImage: `url(${whiteImg})`, height:"200px", }} title="MacBook Air" sub="Supercharged by M2"  text="Available in July" linkText="Learn more" buyText="View pricing"/>
+      
     </div>
   );
 }
