ad700b2a18b500e5e797c977ba3922d67089e36c
nivyapeter <nivyakpeter@gmail.com>
2022-07-04 23:45:36 +0530

    feat: added build component with data

---
+++ turbu-repo-site/src/constant.ts
@@ -0,0 +1,58 @@
+import Card1 from "./Assets/Card1.svg";
+import Card2 from "./Assets/Card2.svg";
+import Card3 from "./Assets/Card3.svg";
+import Card4 from "./Assets/Card4.svg";
+import Card5 from "./Assets/Card5.svg";
+import Card6 from "./Assets/Card6.svg";
+import Card7 from "./Assets/Card7.svg";
+import Card8 from "./Assets/Card8.svg";
+import Card9 from "./Assets/Card9.svg";
+
+export const buildItems = [
+  {
+    image: {Card1},
+    title: "Incremental builds",
+    subTitle: "Building once is painful enough, Turborepo will remember what you've built and skip the stuff that's already been computed.",
+  },
+  {
+    image: {Card2},
+    title: "Incremental builds",
+    subTitle: "Building once is painful enough, Turborepo will remember what you've built and skip the stuff that's already been computed.",
+  },
+  {
+    image: {Card3},
+    title: "Incremental builds",
+    subTitle: "Building once is painful enough, Turborepo will remember what you've built and skip the stuff that's already been computed.",
+  },
+  {
+    image: {Card4},
+    title: "Incremental builds",
+    subTitle: "Building once is painful enough, Turborepo will remember what you've built and skip the stuff that's already been computed.",
+  },
+  {
+    image: {Card5},
+    title: "Incremental builds",
+    subTitle: "Building once is painful enough, Turborepo will remember what you've built and skip the stuff that's already been computed.",
+  },
+  {
+    image: {Card6},
+    title: "Incremental builds",
+    subTitle: "Building once is painful enough, Turborepo will remember what you've built and skip the stuff that's already been computed.",
+  },
+  {
+    image: {Card7},
+    title: "Incremental builds",
+    subTitle: "Building once is painful enough, Turborepo will remember what you've built and skip the stuff that's already been computed.",
+  },
+  {
+    image: {Card8},
+    title: "Incremental builds",
+    subTitle: "Building once is painful enough, Turborepo will remember what you've built and skip the stuff that's already been computed.",
+  },
+  {
+    image: {Card9},
+    title: "Incremental builds",
+    subTitle: "Building once is painful enough, Turborepo will remember what you've built and skip the stuff that's already been computed.",
+  },
+];
+
